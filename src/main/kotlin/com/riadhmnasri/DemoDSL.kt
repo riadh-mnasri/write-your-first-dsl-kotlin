4e0302287a8d465108a043049073be00fd8860bc
Riadh MNASRI <riadh.mnasri@gmail.com>
2022-10-23 13:34:49 +0200

    feat(DSL Marker) Polish

--- src/main/kotlin/com/riadhmnasri/DemoDSL.kt
+++ src/main/kotlin/com/riadhmnasri/DemoDSL.kt
@@ -1,14 +1,14 @@
 package com.riadhmnasri
 
 fun main() {
-    print("Please enter the filename for schedule:")
+    print("Please enter the filename for conference:")
     val fileName = readLine()
     val dsl = java.io.File(fileName).readText()
     val scriptEngine =
         javax.script.ScriptEngineManager().getEngineByExtension("kts")
     try {
         val result = scriptEngine.eval("""
-import com.agiledeveloper.dsl.schedule
+import com.riadhmnasri
 $dsl
 """)
         println("Result of evaluation of the DSL:")
