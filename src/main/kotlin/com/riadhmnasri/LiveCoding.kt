3bf7deb9d6f945e6f1019d5943acecbec77ad9f6
Riadh MNASRI <riadh.mnasri@gmail.com>
2022-11-03 19:17:22 +0100

    chore(DSL) Livecoding live session - livecoding code

--- src/main/kotlin/com/riadhmnasri/LiveCoding.kt
+++ src/main/kotlin/com/riadhmnasri/LiveCoding.kt
@@ -1,46 +1,56 @@
 package com.riadhmnasri
 
+import kotlin.math.roundToInt
 
 
 fun main() {
     // Operator overloading
+    val kotlinBook = Book("id1", "Programming Kotlin", 40.0)
+    val dslBook = Book("id2", "Kotlin DSL", 35.0)
+    println(kotlinBook + dslBook)
 
     // type alias
+    mytasks name "DSL Todolist"
 
     // lambda Out of parentheses // transformC
+    transformCustom("Hello Kotlin") { text -> text.uppercase() }
 
     // Infix functions
+    println("Hello Kotlin" shouldBeEqualTo "Hello Kotlin")
+    println("Hello Kotlin" shouldBeEqualTo "Hello")
 
     // Extension functions
+    println(kotlinBook.rate(5))
 
     // Lambda with receiver // applyC Book
+    kotlinBook.applyCustom {
+        println(title.uppercase())
+        println(price.roundToInt())
+    }
 
     // Demo DSL
 
 }
 
-private fun Book.applyC(func: Book.() -> Unit): Book {
+private fun Book.applyCustom(func: Book.() -> Unit): Book {
     func()
     return this
 }
 
 private fun Book.rate(note: Int) = "*".repeat(note)
 
-private infix fun String.shouldBeEqualTo(text: String): Boolean = this == text
+private infix fun String.shouldBeEqualTo(text: String) = this == text
 
-fun transformC(text: String, func: (String) -> String): String {
-    return func(text)
-}
+fun transformCustom(text: String, func: (String) -> String) = func(text)
 
-private operator fun Book.plus(book: Book) =
-    Book(this.id + book.id, this.title + book.title, this.price + book.price)
+private operator fun Book.plus(book: Book) = Book(this.id + " - " + book.id, this.title + " - " + book.title, this.price + book.price)
 
 data class Book(val id: String, val title: String, val price: Double) {}
 
 typealias mytasks = TodoList
-
 object TodoList {
     infix fun name(text: String) {
         println("TodoList: $text")
     }
 }
+
